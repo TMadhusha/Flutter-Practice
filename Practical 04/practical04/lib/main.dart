487b3cacce7335909743aad2baf8b65fd494e649
TMadhusha <madhusha200069@gmail.com>
2025-06-17 04:43:27 +0530

    commit

---
+++ Practical 04/practical04/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Flutter Application',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            this.title,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.indigoAccent,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                print("Notification icon tapped!");
+              },
+              icon: Icon(Icons.notifications),
+            )
+          ],
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.apple,
+              color: Colors.grey,
+              size: 50,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Hello, Flutter world',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+                onPressed: () {
+                  print("Button pressed");
+                },
+                label: Text('Click Me')),
+          ],
+        )));
+  }
+}
